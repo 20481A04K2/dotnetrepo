7befbf06c7bdeb41b266c194efebc5410f96d98f
SAJJA VAMSI <99644384+20481A04K2@users.noreply.github.com>
2025-05-22 10:31:04 +0530

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,6 +1,10 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Hosting;
+
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello from .NET!");
+app.MapGet("/", () => "✅ Hello from .NET 7 on a GCP VM via Cloud Build and systemd!");
 
-app.Run();
+app.Run("http://0.0.0.0:8080");
